12b94ca16a3df29ee83e194c1bc9f4d88da2f6f1
Peggy Liu <fnoihtlanl@gmail.com>
2017-11-06 21:17:30 +0800

    Test dnd

--- src/router.js
+++ src/router.js
@@ -1,3 +1,4 @@
+import Dnd from './components/Dnd';
 import LandingPage from './components/LandingPage';
 import VideoPage from './components/video/VideoPage';
 
@@ -11,6 +12,11 @@ const routes = [
         path: '/video/:id',
         exact: true,
         component: VideoPage
+    },
+    {
+        path: '/dnd',
+        exact: true,
+        component: Dnd
     }
 ]
 
