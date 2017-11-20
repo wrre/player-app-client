6410ed53cbc7aa6aafe23164cd8741694733a4b6
Peggy Liu <fnoihtlanl@gmail.com>
2017-11-20 18:03:49 +0800

    Add user icon amd modify header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
 import ImageLink from '../share/ImageLink';
 import homeImage from '../../image/home.svg';
+import user from '../../image/user.svg';
 import './Header.css';
 
 class Header extends Component {
   render() {
     const homePath = '/';
     return (
-      <div className="main-Header">
-        <ImageLink className="column-left" src={homeImage} alt="home" path={homePath}/>
-        <h3>{this.props.title || 'Video Player'}</h3>
-      </div>
+      <table className="Header-main">
+        <tr>
+          <td width="30px"><ImageLink className="Header-left" src={homeImage} alt="home" path={homePath}/></td>
+          <td><h3>{this.props.title || 'Video Player'}</h3></td>
+          <td width="30px"><img className="Header-right" src={user} alt="user"/></td>
+        </tr>
+      </table>
     );
   }
 }
