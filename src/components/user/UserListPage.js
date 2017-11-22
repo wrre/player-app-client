22869e9de7b00a6212dbd07752181540ed5b72de
Peggy Liu <fnoihtlanl@gmail.com>
2017-11-22 10:46:02 +0800

    Add header in user list page

--- src/components/user/UserListPage.js
+++ src/components/user/UserListPage.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import Header from '../header/Header';
 
 class UserListPage extends Component {
   render() {
     return (
       <div>
+        <Header/>
         <h1>UserListPage</h1>
       </div>
     );
